b1cf3c347c73cd7e61fc75bbb47384f0c5e784e9
diegof95 <diego.botero@correounivalle.edu.co>
2020-05-06 20:16:45 -0500

    Funcionalidad del boton burger menu en moviles para mostrar u ocultar la navbar.

---
+++ script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const onLoad = () => {
+  const burgerButton = document.getElementById("burger-button");
+  burgerButton.addEventListener('click', switchMenu);
+};
+
+const switchMenu = (ev) => {
+  const navMenu = document.getElementById("menu");
+
+  let burgerButtonIcon = document.getElementById("burger-button-icon")
+                        .getElementsByTagName('use')[0];
+
+  navMenu.classList.contains('active') ? navMenu.classList.remove('active') : navMenu.classList.add('active');
+  
+  if(burgerButtonIcon.getAttribute('xlink:href') == '#icon-menu'){
+    burgerButtonIcon.setAttribute('xlink:href', '#icon-cancel-circle');
+  }
+  else{
+    burgerButtonIcon.setAttribute('xlink:href', '#icon-menu');
+  }
+};
+
