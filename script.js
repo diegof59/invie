b4b415b1ab6500cbca97e02517eb6a840b1a2958
diegof95 <diego.botero@correounivalle.edu.co>
2020-05-24 21:08:03 -0500

    Arreglos para la pág de Precios.

--- script.js
+++ script.js
@@ -8,10 +8,13 @@ const onLoad = () => {
 const switchMenu = (ev) => {
   const navMenu = document.getElementById("menu");
 
-  let burgerButtonIcon = document.getElementById("burger-button-icon")
+  const burgerButtonIcon = document.getElementById("burger-button-icon")
                         .getElementsByTagName('use')[0];
 
-  navMenu.classList.contains('active') ? navMenu.classList.remove('active') : navMenu.classList.add('active');
+  navMenu.classList.contains('active') ?
+      navMenu.classList.remove('active')
+    :
+      navMenu.classList.add('active');
   
   if(burgerButtonIcon.getAttribute('xlink:href') == '#icon-menu'){
     burgerButtonIcon.setAttribute('xlink:href', '#icon-cancel-circle');
